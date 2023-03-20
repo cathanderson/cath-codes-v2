787c13455794a6da3d78de4ac710d3f593756f97
“Catherine <cathanders10@gmail.com>
2023-03-20 16:35:09 -0400

    navbar/hamburger menu

---
+++ app.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector("#hamburger");
+const dropdown = document.querySelector("#dropdown");
+
+hamburger.addEventListener("click", ()=> {
+    dropdown.classList.toggle("hidden")
+})

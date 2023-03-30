c81d8272896d19be5acb696dadeb2cda986aba79
“Catherine <cathanders10@gmail.com>
2023-03-30 19:06:49 -0400

    started dark mode through about section

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector("#hamburger");
 const dropdown = document.querySelector("#dropdown");
+const anchorLinks = document.querySelectorAll("#anchor-link")
 
 hamburger.addEventListener("click", ()=> {
     dropdown.classList.toggle("hidden")
+})
+
+anchorLinks.forEach((link)=> {
+    link.addEventListener("click", () => {
+      dropdown.classList.toggle("hidden");
+    });
 })
 

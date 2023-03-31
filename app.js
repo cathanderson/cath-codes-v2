88b6031b42e100030ee8fb6e10567bbb44f60fa3
“Catherine <cathanders10@gmail.com>
2023-03-31 13:44:26 -0400

    event listeners for dark mode

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector("#hamburger");
 const dropdown = document.querySelector("#dropdown");
-const anchorLinks = document.querySelectorAll("#anchor-link")
+const anchorLinks = document.querySelectorAll("#anchor-link");
+const body = document.querySelector("body");
+const moon = document.querySelector("#moon");
+const sun = document.querySelector("#sun");
+
 
 hamburger.addEventListener("click", ()=> {
     dropdown.classList.toggle("hidden")
@@ -11,4 +15,19 @@ anchorLinks.forEach((link)=> {
       dropdown.classList.toggle("hidden");
     });
 })
+
+moon.addEventListener("click", ()=> {
+    // body.classList.add("dark");
+    body.classList.toggle("dark");
+    body.classList.toggle("bg-orchid-300");
+    body.classList.toggle("bg-orchid-600");
+})
+
+sun.addEventListener("click", () => {
+//   body.classList.remove("dark");
+//   body.classList.add("dark");
+  body.classList.toggle("dark");
+  body.classList.toggle("bg-orchid-300");
+  body.classList.toggle("bg-orchid-600");
+});
 
